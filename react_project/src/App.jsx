9c589fb6535a7855d1e370d0025282d3f121fca8
Varun2024 <marvellous079@gmail.com>
2024-10-13 15:51:35 +0530

    update

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,3 +1,10 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
@@ -7,6 +14,11 @@ import ViewAllJobs from "./components/ViewAllJobs";
 // every react component will return jsx but you can only return only one element with as much as functioning inside it , it must be wrapped inside a single parent element
 // wrapped in a fragment "<></>" 
 // if need to add dynamic values in jsx , use {} and put whatever value inside that
+
+const router = createBrowserRouter(
+  // for home page 
+  createRoutesFromElements(<Route index element/>)
+)
 
 const App = () => {
   return (
--- react_project/src/components/JobListing.jsx
+++ react_project/src/components/JobListing.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 
 import { useState } from "react"
+// importin fontawesome icons 
+import {FaMapMarker} from 'react-icons/fa'
  
 
 // taking job a props from the array in prev file
@@ -28,14 +30,15 @@ const JobListing = ({job}) => {
             <button 
             onClick= {() => setShowFullDescription((prevState) => (!prevState))}className="text-indigo-500 mb-5 hover:text-indigo-600"> {showFullDescription ? 'Less' : 'More'}</button>
 
-            <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
+            <h3 className="text-indigo-500 mb-2 mr-1">{job.salary}</h3>
 
             <div className="border border-gray-100 mb-5"></div>
 
             <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                <i className="fa-solid fa-location-dot text-lg"></i>
-                {job.location}
+                    {/* use inline when need to put elements side by side */}
+                    <FaMapMarker className="inline text-lg mb-1"/>
+                    {job.location}
                 </div>
                 <a
                 href={`/job/${job.id}`}
