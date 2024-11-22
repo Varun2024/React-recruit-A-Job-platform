4d2ded11e6e7f0d77de537f922da40ed2266e9c8
Varun2024 <marvellous079@gmail.com>
2024-11-22 15:21:51 +0530

    loaderandfetchupdate

--- react_project/src/components/JobListings.jsx
+++ react_project/src/components/JobListings.jsx
@@ -1,26 +1,55 @@
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing'
-
+import Spinner from './spinner';
 // ishome is used as props for diff situation of sowing jobs at home page and on jobs page, by default its false so itll show all jobs but when the props is set to true showing 3 jobs at home page
+
 // eslint-disable-next-line react/prop-types
 const JobListings = ({isHome = false}) => {
+    const [jobs , setJobs] = useState([]);//default value in useState is empty bcoz we will request using useEffect from the server and fill the empty array  
+    
+    // eslint-disable-next-line no-unused-vars
+    const [loading , setLoading] = useState(true) // for loading animation while requesting and after the fetching the default value will be false
 
-   const jobListings = isHome ? jobs.slice(0,3) : jobs 
+// fetching the data from the server and setting state of the job which currently empty(empty array) with the fetched data
+    useEffect(() => {
+        const fetchJobs = async () =>{
+            const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs' // _limit is the key here
+            try {
+                const res = await fetch(apiUrl)
+                const data = await res.json()//storing fetched data
+                setJobs(data)
+            } catch (error) {
+                console.log('Error fetching data', error)
+            }finally{
+                setLoading(false) // always execute as 
+            }
+        } 
+        fetchJobs()
+    }, []);
+
+
   return (
     //  {/* <!-- Browse Jobs --> */}
     <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome ? 'Recent Jobs' : 'Browse jobs'}
-        </h2>
+            <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                {isHome ? 'Recent Jobs' : 'Browse jobs'}
+            </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/* mapping the array from jobs.json using map method , prevents repetition of elements and outputting bunch pf html code */}
-            {jobListings.map((job)=> (
-                // list requires a key value(unique) and another joblisting element for individual element
-                <JobListing key={job.id} job={job}/>
-            ))}
-            </div>   
+            
+                {loading ? (
+                    <Spinner loading={loading} />
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {/* mapping the array from jobs.json using map method , prevents repetition of elements and outputting bunch pf html code */}
+                        {jobs.map((job)=> (
+                            // list requires a key value(unique) and another joblisting element for individual element
+                            <JobListing key={job.id} job={job}/>
+                        ))}
+                    </div>    
+                )}
+                
+            
         </div>
    </section>
   )
--- react_project/src/components/navbar.jsx
+++ react_project/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 
 const Navbar = () => {
-  const linkclass = ({isActive}) => isActive ? "text-white bg-black hover:bg-gray-600 hover:text-white rounded-md px-3 py-2" : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+  const linkclass = ({isActive}) => isActive ? "text-white bg-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2" : 'text-white hover:bg-gray-800 hover:text-white rounded-md px-3 py-2'
 
   return (
       <nav className="bg-indigo-700 border-b border-indigo-500">
